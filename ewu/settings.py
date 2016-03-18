24307701e88456af607e4d140d5c17a6ce5ef380
pixiu <pixiuwait@163.com>
2016-03-18 23:01:01 +0800

    Set database and timezone settings

--- ewu/settings.py
+++ ewu/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'ewu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ewu',
+        'PASSWORD': '123456789',
     }
 }
 
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'PRC'
 
 USE_I18N = True
 
