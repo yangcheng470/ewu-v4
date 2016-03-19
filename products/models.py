f4708218fdc59f378c4dfb9c04b73b9bf35d6671
pixiu <pixiuwait@163.com>
2016-03-19 09:59:22 +0800

    Add pub_date and valid fields

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Product(models.Model):
@@ -41,6 +42,11 @@ class Product(models.Model):
     imgs=models.CharField(max_length=200)  
 
     content=models.TextField()
+
+    pub_date=models.DateTimeField(default=timezone.now())
+
+    # If sold, valid field can be set to False
+    valid=models.BooleanField(default=True)
 
     # return content for admin interface
     def get_content(self):
