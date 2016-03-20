28a7ad62da11feb0b542e8a3f26a8aadb829b3d3
pixiu <pixiuwait@163.com>
2016-03-20 20:24:50 +0800

    Create user center(ucenter) link

--- products/views.py
+++ products/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 def index(request):
     return HttpResponse('<h1>Product Index Here.</h1>')
+
+
+def detail(request,product_id):
+    try:
+        product=Product.objects.get(pk=product_id)
+    except Product.DoesNotExist:
+        return HttpResponse('Product does not exist.')
+    else:
+        return HttpResponse('<h1>Product name: %s. </h1>' % product.name)
