4cb1b2f2d063c3f9fb73c201865a80a8ad08ba73
pixiu <pixiuwait@163.com>
2016-03-20 20:43:07 +0800

    Create help(mainly show some help tips) and about(mainly introduce our team) links

--- index/urls.py
+++ index/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns=[
         url(r'^$',views.index,name='index'),
+        url(r'^help/$',views.help,name='help'),
+        url(r'^about/$',views.about,name='about'),
 ]
--- index/views.py
+++ index/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>Index Page Here.</h1>')
+
+
+def help(request):
+    return HttpResponse('<h1>help Page Here.</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>about Page Here.</h1>')
