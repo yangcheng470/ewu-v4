892710dcb03242b023bd86cf18aa09ad73a783f7
pixiu <pixiuwait@163.com>
2016-03-25 23:45:10 +0800

    Add delete-account page. This function is necessary though dangerous.

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns=[
         url(r'^$',views.index,name='index'),
         url(r'^reset-password/$',views.reset_passwd,name='reset_passwd'),
         url(r'^reset-password/success$',views.reset_passwd_success,name='reset_passwd_success'),
+        url(r'^delete-account/$',views.delete_account,name='delete_account'),
+        url(r'^delete-account/success/$',views.delete_account_success,name='delete_account_success'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -12,3 +12,16 @@ def reset_passwd(request):
 
 def reset_passwd_success(request):
     return HttpResponse('Reset password success page here.')
+
+
+def delete_account(request):
+    return HttpResponse('Delete account page here.')
+
+
+# Attention: This action is dangerous. Action should be verified by authorizing password.
+# After delete account, all relative products should be removed, too.
+# All data will be deleted from database physically.
+# Why this action is provided? This is a problem just in China, not in foreign.
+# This should be the necessary and basic( rather extra ) right.
+def delete_account_success(request):
+    return HttpResponse('Delete account success page here.')
