45f410edd9b96470c2ec0cc03fe7032ecb8f47e3
pixiu <pixiuwait@163.com>
2016-03-25 23:34:09 +0800

    Add reset-password page

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns=[
         url(r'^$',views.index,name='index'),
+        url(r'^reset-password/$',views.reset_passwd,name='reset_passwd'),
+        url(r'^reset-password/success$',views.reset_passwd_success,name='reset_passwd_success'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,3 +6,9 @@ def index(request):
     return HttpResponse('Ucenter page here.')
 
 
+def reset_passwd(request):
+    return HttpResponse('Reset password page here.')
+
+
+def reset_passwd_success(request):
+    return HttpResponse('Reset password success page here.')
