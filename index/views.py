8d007fb805bf690a3a7f883f330f601c544e514e
karlind <inner.roe.deer@gmail.com>
2016-06-17 21:17:17 +0800

    Take apart's main content to index.html

--- index/views.py
+++ index/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "base.html", {})
+    return render(request, "index.html", {})
 
 
 # Search result page, keyword conveyed by GET method
