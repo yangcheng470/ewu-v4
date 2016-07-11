655d21ce32d1d27da94d3720f34078cd119798a5
karlind <inner.roe.deer@gmail.com>
2016-07-11 14:14:43 +0800

    Add functions to change product status multily

--- products/admin.py
+++ products/admin.py
@@ -3,12 +3,24 @@ from .models import Product
 
 # Register your models here.
 
+def make_sold(modeladmin, request, queryset):
+    queryset.update(status='sold')
+
+make_sold.short_description = '标记为售出'
+
+
+def make_sale(modeladmin, request, queryset):
+    queryset.update(status='sale')
+
+make_sale.short_description = '标记为在售'
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display=['id','name','owner','valid','status','category','price','condition','pub_date','get_content']
     list_display_links = ['id', 'name']
     ordering=['id']
     save_as = True
+    actions = [make_sold, make_sale]
 
 
 admin.site.register(Product,ProductAdmin)
